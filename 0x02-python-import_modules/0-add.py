15bd7ed3e51f1813d33f111c6dca115c4562127f
obimiz <mbanuobinna.com>
2024-02-01 08:23:06 +0100

    add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,11 +3,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
+#import the add function from add_0.py
+    from add_0 import add
     a = 1
     b = 2
 
 #import the add function from add_0.py
-    from add_0 import add
 
 #Use the variable a and b as an argument and print the result
     print("{} + {} = {}".format(a, b, add(a, b)))
