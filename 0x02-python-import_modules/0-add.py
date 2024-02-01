3037e5c2f88c8495bbf4d0658d3b108f5ddde8d3
obimiz <mbanuobinna.com>
2024-02-01 12:39:27 +0100

    import add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+
+if __name__ == "__main__":
+    from add_0 import add
 
     a = 1
     b = 2
+    result = add(a, b)
 
-if __name__ == "__main__":
-
-    return a + b
-
-
+    print("{} + {} = {}".format(a, b, result))
