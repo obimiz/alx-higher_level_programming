d27afd61034e282d9282f132017a132513a94377
obimiz <mbanuobinna.com>
2024-02-01 11:36:12 +0100

    import add function

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
-
-#import the add function from add_0.py
-    from add_0 import add
     a = 1
     b = 2
 
 if __name__ == "__main__":
-#import the add function from add_0.py
 
-#Use the variable a and b as an argument and print the result
-return a + b
+    return a + b
 
 
