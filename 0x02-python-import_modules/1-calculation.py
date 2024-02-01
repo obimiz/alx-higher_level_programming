2bf77b798a9e97d03d8c72d8fcdcd5f0609fcfcb
obimiz <mbanuobinna.com>
2024-02-01 14:39:27 +0100

    calculator_module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,18 +1,16 @@
-from calculator_1 import add, sub, mul, div
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-#Examples values
+    a = 10
+    b = 5
 
-a = 10
-b = 5
+    result_add = add(a, b)
+    result_subtract = sub(a, b)
+    result_multiply = mul(a, b)
+    result_divide = div(a, b)
 
-# Perform calculations using imported functions
-result_add = add(a, b)
-result_subtract = sub(a, b)
-result_multiply = mul(a, b)
-result_divide = div(a, b)
-
-# print the result
-print("Addition: {} + {} = {}".format(a, b, result_add))
-print("Subtraction: {} - {} = {}".format(a, b, result_subtract))
-print("Multiplication: {} * {} = {}".format(a, b, result_multiply))
-print("Division: {} / {} = {}".format(a, b, result_divide))
+print("{} + {} = {}".format(a, b, result_add))
+print("{} - {} = {}".format(a, b, result_subtract))
+print("{} * {} = {}".format(a, b, result_multiply))
+print("{} / {} = {}".format(a, b, result_divide))
