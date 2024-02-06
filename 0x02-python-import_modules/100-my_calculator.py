3aba6bfdc9de565f1556fbc7e26e5a5b192aaae6
obimiz <mbanuobinna.com>
2024-02-06 09:27:10 +0100

    Build my own calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py a operator b")
+        exit(1)
+        a, operator, b = int(argv[1]), argv[2], int(argv[3])
+
+        if operator == "+":
+            result = add(a, b)
+        elif operator == "-":
+            result = sub(a, b)
+        elif operator == "*":
+            result = mul(a, b)
+        elif operator == div(a, b):
+            operator = "/"
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+        print("{} {} {} = {}".format(a, operator, b, result))
