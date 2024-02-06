c3dbcc6bcd63065cca018ee9fd1c4b63fdfe4f00
obimiz <mbanuobinna.com>
2024-02-06 10:55:39 +0100

    Calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -19,4 +19,5 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-        print("{} {} {} = {}".format(a, operator, b, result))
+
+    print("{} {} {} = {}".format(a, operator, b, result))
