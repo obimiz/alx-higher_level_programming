dcbc23d23600b9d2433af39d4c17e04248ee3df8
obimiz <mbanuobinna.com>
2024-02-16 13:29:11 +0100

    a function that adds all unique integers in a list (only once for each integer)

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def unique_add(my_list=[]):
-    unique_set = set(my_list)
-    return sum(unique_set)
+def uniq_add(my_list=[]):
+    uniq_sum = 0
+    uniq_elements = set()
+    
+    for num in my_list:
+        if num not in uniq_elements:
+            uniq_sum += num
+            uniq_elements.add(num)
+
+    return uniq_sum
