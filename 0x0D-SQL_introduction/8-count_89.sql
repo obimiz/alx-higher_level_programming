5bfb98ba07cd2f0f5308f825fd2d158c48bab7ea
obimiz <mbanuobinna.com>
2024-03-14 11:18:59 +0100

    a script that create a table and adds multiple rows

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- a script that displace the number of a records
 SELECT COUNT(*)
 FROM first_table
-WHERE id = 89;
+WHEREid = 89;
---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- a script that creates a table and adds multiple rows
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
