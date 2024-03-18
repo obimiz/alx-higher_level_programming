85241f2124c225a5987e156d0d15ab9775530a03
obimiz <mbanuobinna.com>
2024-03-18 13:33:00 +0100

    say by name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,8 @@
+-- a script that lists all records of the table second_table of the database
+-- Don’t list rows without a name value
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score
+SELECT name, score
+FROM second_table
+WHERE name != ""
+ORDER BY score DESC;
