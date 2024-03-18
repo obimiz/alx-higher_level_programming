6de65b341a5b22c73a6ae9ca75bfedf74d32bea9
obimiz <mbanuobinna.com>
2024-03-18 17:27:27 +0100

    Temperatures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- display the city with the highest average
+-- temperature between July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
