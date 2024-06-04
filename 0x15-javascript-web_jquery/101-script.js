5f6ae600f661d3afbb30edfb3b1b466b1622dad8
obimiz <mbanuobinna.com>
2024-06-04 12:57:16 +0100

    Write a JavaScript script that adds, removes and clears LI elements from a list when the user clicks:

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,15 @@
+$(function () {
+	$('#add_item').click(function () {
+		$('.my_list').append('<li>Item</li>');
+	});
+
+	$('#remove_item').click(function () {
+		$('.my_list li:last-child').remove();
+	});
+
+	$('#clear_list').click(function () {
+		$('.my_list').empty();
+	});
+});
+
+
