a79169212c8d07a930349c0bef9d6e7b733c5fa5
obimiz <mbanuobinna.com>
2024-06-19 12:24:33 +0100

    print 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const c = "C is fun";
-const python = "python is cool";
-const javaScript = "javaScript is amazing";
+const c = 'C is fun';
+const python = 'python is cool';
+const javaScript = 'javaScript is amazing';
 
 console.log(c);
 console.log(python);
