62a5fc3c78f87b51f89db4d9b3804fb6b708a3e4
obimiz <mbanuobinna.com>
2024-06-20 23:51:52 +0100

    script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const c = 'C is fun';
-const python = 'python is cool';
-const javaScript = 'javaScript is amazing';
 
-console.log(c);
-console.log(python);
-console.log(javaScript);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
+
